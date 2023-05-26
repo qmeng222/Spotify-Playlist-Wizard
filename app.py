48c9c331c86d3f855b0aca29ee002d172d6f5949
Qingying Meng <qmeng222@gmail.com>
2023-05-25 22:01:11 -0700

    retrieved the id the searched track

--- app.py
+++ app.py
@@ -2,11 +2,25 @@ import openai
 from dotenv import dotenv_values
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import pprint
+
+"""
+REFERENCE ONLY:
+[
+  {"song": "Everybody Hurts", "artist": "R.E.M."},
+  {"song": "Nothing Compares 2 U", "artist": "Sinead O'Connor"},
+  {"song": "Tears in Heaven", "artist": "Eric Clapton"},
+  {"song": "Hurt", "artist": "Johnny Cash"},
+  {"song": "Yesterday", "artist": "The Beatles"}
+]
+"""
 
 config = dotenv_values(".env")
 openai.api_key = config["OPENAI_API_KEY"]
 
+# sp object is an instance of the spotipy.Spotify class, which represents a client for interacting with the Spotify API.:
 sp = spotipy.Spotify(
+    # auth_manager is set to an instance of SpotifyOAuth:
     auth_manager=SpotifyOAuth(
         client_id=config["SPOTIFY_CLIENT_ID"],
         client_secret=config["SPOTIFY_CLIENT_SECRET"],
@@ -15,4 +29,7 @@ sp = spotipy.Spotify(
 )
 
 current_user = sp.current_user()
-print(current_user)
+assert current_user is not None
+# print(current_user)
+search_results = sp.search(q="Uptown Funk", type="track", limit=10)
+pprint.pprint(search_results["tracks"]["items"][0]["id"])
